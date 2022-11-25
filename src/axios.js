ceee293e73fc03ff1d78e00f5d3b08acda487120
Prakash7708 <bprakash2902@gmail.com>
2022-11-25 21:44:57 +0530

    render-server

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 let instance=axios.create({
      //baseURL:"http://localhost:3001"
-     baseURL:"https://hallbooking-server.herokuapp.com"
+     //baseURL:"https://hallbooking-server.herokuapp.com"
+     baseURL:"https://hallbooking-server.onrender.com/"
 })
 
 export default instance;
